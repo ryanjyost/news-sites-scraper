93faf0e86e1d2f20694d8b9bdf9f2be3177bbfd4
Ryan Yost <ryost217@gmail.com>
2019-11-21 21:13:19 -0600

    some code cleanup

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-const URL =
-  "mongodb://skoosh:skoosh2016@ds157499.mlab.com:57499/media-bias-map";
+const URL = process.env.DB_URL;
 
 // const URL =
 //   "mongodb://skoosh:skoosh2002@ds127362.mlab.com:27362/newsbie-sandbox";
